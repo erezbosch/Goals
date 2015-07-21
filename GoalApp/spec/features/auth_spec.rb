9e969c6d295496bba1d2a50971c5da9a1afd709a
App Academy Student <student@appacademy.io>
2015-07-20 18:05:00 -0700

    finishing touches

--- GoalApp/spec/features/auth_spec.rb
+++ GoalApp/spec/features/auth_spec.rb
@@ -1,51 +1,54 @@
 require 'spec_helper'
 require 'rails_helper'
+describe 'auth' do
+  let(:test) { create :user }
+  
+  feature "the signup process" do
 
-feature "the signup process" do
 
-  it "has a new user page" do
-    visit new_user_url
-    expect(page).to have_content "Sign up"
-  end
+    it "has a new user page" do
+      visit new_user_url
+      expect(page).to have_content "Sign up"
+    end
 
-  feature "signing up a user" do
+    feature "signing up a user" do
 
-    it "shows username on the homepage after signup" do
-      sign_up_as_test
+      it "shows username on the homepage after signup" do
+        sign_up(test)
 
-      expect(page).to have_content "test"
+        expect(page).to have_content test.username
+      end
     end
   end
-end
 
-feature "logging in" do
+  feature "logging in" do
 
-  it "shows username on the homepage after login" do
-    sign_up_as_test
+    it "shows username on the homepage after login" do
+      sign_up(test)
 
-    click_on "Sign out"
+      click_on "Sign out"
 
-    visit new_session_url
-    fill_in 'username', with: "test"
-    fill_in 'password', with: "password123"
-    click_on "Sign in"
+      visit new_session_url
+      sign_in(test)
 
-    expect(page).to have_content "test"
+      expect(page).to have_content test.username
+    end
   end
-end
 
-feature "logging out" do
+  feature "logging out" do
 
-  it "begins with logged out state" do
-      visit root_url
-      expect(page).to have_content "Sign in"
-      end
+    it "begins with logged out state" do
+        visit root_url
+        expect(page).to have_content "Sign in"
+        end
 
-  it "doesn't show username on the homepage after logout" do
-    sign_up_as_test
+    it "doesn't show username on the homepage after logout" do
+      sign_up(test)
 
-    click_on "Sign out"
-    expect(page).to_not have_content "test"
+      click_on "Sign out"
+      expect(page).to_not have_content test.username
+    end
+
   end
 
 end
